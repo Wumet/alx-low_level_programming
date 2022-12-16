d197c2b9bc7bc753b1a6d8fb27bfdcae86062f33
root <root@localhost.localdomain>
2022-12-16 15:26:50 +0000

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - prints lower case alphabets excluding q and e
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	char lc;	
+	for (lc = 'a'; lc = 'z'; lc++)
+	if (lc != 'q' && lc != 'e')
+{
+		putchar(lc);
+		putchar('\n');
+}
+	return (0);
+}
