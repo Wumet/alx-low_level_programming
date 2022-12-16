4778a9830a0096f0c0f63e23c5d4df3fe7dcf89e
root <root@localhost.localdomain>
2022-12-16 16:00:32 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - prints digits 0 to 9
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0, n <= 9, n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
